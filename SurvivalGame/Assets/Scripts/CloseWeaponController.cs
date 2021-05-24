1aab7f0feb1486bde54898de38d987b545ad5269
MomDra <yem9087@koreatech.ac.kr>
2021-05-25 02:52:24 +0900

    CloseWeapon - Axe, Pickaxe

---
+++ SurvivalGame/Assets/Scripts/CloseWeaponController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CloseWeaponController : MonoBehaviour
+{
+    // 현재 장착된 Hand형 타입 무기
+    [SerializeField]
+    protected CloseWeapon currentWeapon;
+
+    // 공격중
+    protected bool isAttack;
+    protected bool isSwing;
+
+    protected RaycastHit hitinfo;
+
+    protected void TryAttack()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (!isAttack)
+            {
+                // 코루틴 실행
+                StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+
+    protected IEnumerator AttackCoroutine()
+    {
+        isAttack = true;
+        currentWeapon.anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(currentWeapon.attackDelayA);
+        isSwing = true;
+
+        StartCoroutine(HitCoroutine());
+
+        yield return new WaitForSeconds(currentWeapon.attackDelayB);
+        isSwing = false;
+
+        yield return new WaitForSeconds(currentWeapon.attackDelay - currentWeapon.attackDelayA - currentWeapon.attackDelayB);
+
+        isAttack = false;
+    }
+
+    // 미완성 = 추상 코루틴
+    protected abstract IEnumerator HitCoroutine();
+    
+    protected bool checkObject()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitinfo, currentWeapon.range))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // 완성 함수이지만, 추가 편집한 함수
+    public virtual void CloseWeaponChange(CloseWeapon _closeWeapon)
+    {
+        if (WeaponManager.currentWeapon != null)
+            WeaponManager.currentWeapon.gameObject.SetActive(false);
+
+        currentWeapon = _closeWeapon;
+        WeaponManager.currentWeapon = currentWeapon.transform;
+        WeaponManager.currentWeaponAnim = currentWeapon.anim;
+
+        currentWeapon.transform.localPosition = Vector3.zero;
+        currentWeapon.gameObject.SetActive(true);
+    }
+}
--- SurvivalGame/Assets/Scripts/WeaponManager.cs
+++ SurvivalGame/Assets/Scripts/WeaponManager.cs
@@ -26,28 +26,51 @@ public class WeaponManager : MonoBehaviour
     [SerializeField]
     Gun[] guns;
     [SerializeField]
-    Hand[] hands;
+    CloseWeapon[] hands;
+    [SerializeField]
+    CloseWeapon[] axes;
+    [SerializeField]
+    CloseWeapon[] pickaxes;
 
     // 관리 차원에서 쉽게 무기 접근이 가능하도록 만듬
     Dictionary<string, Gun> gunDictionary = new Dictionary<string, Gun>();
-    Dictionary<string, Hand> handDictionary = new Dictionary<string, Hand>();
+    Dictionary<string, CloseWeapon> handWeaponDictionary = new Dictionary<string, CloseWeapon>();
+    Dictionary<string, CloseWeapon> axeWeaponDictionary = new Dictionary<string, CloseWeapon>();
+    Dictionary<string, CloseWeapon> pickaxeWeaponDictionary = new Dictionary<string, CloseWeapon>();
+
 
     // 필요한 컴포넌트
     [SerializeField]
     GunController theGunController;
     [SerializeField]
     HandController theHandController;
+    [SerializeField]
+    AxeController theAxeController;
+    [SerializeField]
+    PickaxeController thePickaxeController;
 
     private void Start()
     {
-        for (int i = 0; i < guns.Length; i++)
+        int i = 0;
+
+        for (i = 0; i < guns.Length; i++)
         {
             gunDictionary.Add(guns[i].gunName, guns[i]);
         }
 
-        for (int i = 0; i < hands.Length; i++)
+        for (i = 0; i < hands.Length; i++)
         {
-            handDictionary.Add(hands[i].handName, hands[i]);
+            handWeaponDictionary.Add(hands[i].closeWeaponName, hands[i]);
+        }
+
+        for (i = 0; i < axes.Length; i++)
+        {
+            axeWeaponDictionary.Add(axes[i].closeWeaponName, axes[i]);
+        }
+
+        for (i = 0; i < pickaxes.Length; i++)
+        {
+            pickaxeWeaponDictionary.Add(pickaxes[i].closeWeaponName, pickaxes[i]);
         }
     }
 
@@ -57,8 +80,12 @@ public class WeaponManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
                 StartCoroutine(ChangeWeaponCoroutine("GUN", "SubMachineGun1"));
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-                StartCoroutine(ChangeWeaponCoroutine("HAND", "RawHand"));
+            else if(Input.GetKeyDown(KeyCode.Alpha2))
+                StartCoroutine(ChangeWeaponCoroutine("AXE", "Axe"));
+            else if(Input.GetKeyDown(KeyCode.Alpha3))
+                StartCoroutine(ChangeWeaponCoroutine("PICKAXE", "Pickaxe"));
+            else if (Input.GetKeyDown(KeyCode.Alpha4))
+                StartCoroutine(ChangeWeaponCoroutine("HAND", "Hand"));
         }
     }
 
@@ -87,6 +114,12 @@ public class WeaponManager : MonoBehaviour
                 theGunController.CancelReload();
                 GunController.isActivate = false;
                 break;
+            case "AXE":
+                AxeController.isActivate = false;
+                break;
+            case "PICKAXE":
+                PickaxeController.isActivate = false;
+                break;
             case "HAND":
                 HandController.isActivate = false;
                 break;
@@ -97,8 +130,11 @@ public class WeaponManager : MonoBehaviour
     {
         if (_type == "GUN")
             theGunController.GunChange(gunDictionary[_name]);
-
+        else if (_type == "AXE")
+            theAxeController.CloseWeaponChange(axeWeaponDictionary[_name]);
+        else if(_type == "PICKAXE")
+            thePickaxeController.CloseWeaponChange(pickaxeWeaponDictionary[_name]);
         else if (_type == "HAND")
-            theHandController.HandChange(handDictionary[_name]);       
+            theHandController.CloseWeaponChange(handWeaponDictionary[_name]);  
     }
 }
