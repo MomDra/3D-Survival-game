2f548f3d8e8c5de3cfbbbf820fa6dec74004c74f
MomDra <yem9087@koreatech.ac.kr>
2021-06-21 16:37:24 +0900

    Day and night

--- SurvivalGame/Assets/Scripts/DayAndNight.cs
+++ SurvivalGame/Assets/Scripts/DayAndNight.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class DayAndNight : MonoBehaviour
 {
+    [SerializeField]
+    float secondPerRealTimeSecond;
+
+    bool isNight;
+
+    [SerializeField]
+    float fogDensityCalc; // 증감량 비율
+
+    [SerializeField]
+    float nightFogDensity; // 밤 상태의 Fog 밀도
+    float dayFogDensity; // 낮 상태의 fog 밀도
+    float currentFogDensity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dayFogDensity = RenderSettings.fogDensity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.right, 0.1f * secondPerRealTimeSecond * Time.deltaTime);
+
+        if (transform.eulerAngles.x >= 170)
+            isNight = true;
+        else if (transform.eulerAngles.x <= 10)
+            isNight = false;
+
+        if (isNight)
+        {
+            if(currentFogDensity <= nightFogDensity)
+            {
+                currentFogDensity += 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+        else
+        {
+            if (currentFogDensity >= dayFogDensity)
+            {
+                currentFogDensity -= 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+            
     }
 }
